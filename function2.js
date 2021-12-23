6260b0a0be73c98bbdcb1a7be2195a66ab3eb7ef
Naru-hub <1208ny0324@gmail.com>
2021-12-23 22:44:41 +0900

    closure

--- function2.js
+++ function2.js
@@ -36,6 +36,20 @@
 // outerFunc();
 
 
+// function closure(init) {
+//   var counter = init;
+
+//   return function() {
+//     return ++counter;
+//   }
+// }
+
+// var myClosure = closure(1);
+// console.log(myClosure());  //2
+// console.log(myClosure());  //3
+// console.log(myClosure());  //4
+
+
 function closure(init) {
   var counter = init;
 
@@ -44,7 +58,12 @@ function closure(init) {
   }
 }
 
-var myClosure = closure(1);
-console.log(myClosure());  //2
-console.log(myClosure());  //3
-console.log(myClosure());  //4
+var myClosure1 = closure(1);
+var myClosure2 = closure(100);
+
+console.log(myClosure1());  //2
+console.log(myClosure2());  //101
+console.log(myClosure1());  //3
+console.log(myClosure2());  //102
+
+
